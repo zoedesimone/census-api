f844709cfc0455453ce42c76489e8c5fa11827c0
zoedesimone <56321398+zoedesimone@users.noreply.github.com>
2023-02-15 21:08:53 -0500

    Updated examples

--- example/example.py
+++ example/example.py
@@ -1,18 +1,25 @@
 from pathlib import Path
 import os 
+import censusapi.censusgdf as cdf
+import geopandas as gpd
 
 
 #Example Filepath
 THIS_DIR = Path(__file__).parent
-my_data_path = THIS_DIR /"data"/"input"/ 'testdata.geojson'
+in_path = THIS_DIR /"data"/"input"/ 'testdata.geojson'
 my_output_path = THIS_DIR /"data"/"output"/ 'resultdata.geojson'
 key = os.environ.get('secretUser')
 
-import censusapi.censusgdf as cdf
 
 #Example 1: Augmenting shapefile given an input and output filepath
-cdf.add_census_to_geojson(my_data_path,my_output_path, key)
+cdf.add_census_to_geojson(in_path,my_output_path, key)
 
-#Example 2: Augmenting shapefile given an input and output filepath
-cdf.add_census_to_geojson(my_data_path,my_output_path, key)
+#Example 2: Augmenting shapefile given an existing geodataframe
+df = gpd.read_file(in_path)
+cdf.add_census_to_geojson_df(df, key)
+
+#Example 3: Augmenting shapefile given an existing geodataframe with custom census variables
+df = gpd.read_file(in_path)
+optional_census_variables = ('NAME','B01001_001E')
+cdf.add_census_to_geojson_df(df, key, optional_census_variables)
 
